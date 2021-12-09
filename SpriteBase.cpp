5a0d2fba36ec1e280a1958244a9e9609756eeec9
guapisolo <guapisolo@qq.com>
2021-12-09 16:12:32 +0800

    original version

---
+++ SpriteBase.cpp
@@ -0,0 +1,45 @@
+#include "SpriteBase.h"
+
+SpriteBase::SpriteBase(int x, int y, int width, int height, int dx, int dy, ACL_Image *img, rect r1)
+{
+	this->x = x;
+	this->y = y;
+	this->width = width;
+	this->height = height;
+	this->dx = dx;
+	this->dy = dy;
+	this->img = img;
+	//this->hitpoint = 1;
+	r = r1;
+}
+SpriteBase::SpriteBase(SpriteBase & sprite)
+{
+	x = sprite.x;
+	y = sprite.y;
+	width = sprite.width;
+	height = sprite.height;
+	dx = sprite.dx;
+	dy = sprite.dy;
+	img = sprite.img;
+	r = sprite.r;
+	//hitpoint = sprite.hitpoint;
+}
+SpriteBase::~SpriteBase()
+{
+
+}
+
+
+void SpriteBase::drawSprite(int width, int height)
+{
+	putImageScale(img, x, y, width, height);
+}
+void SpriteBase::drawSprite()
+{
+	putImageScale(img, x, y, width, height);
+}
+rect SpriteBase::getRect()
+{
+	rect r = { x, y, width, height };
+	return r;
+}
---
+++ readme.md
@@ -0,0 +1,12 @@
+gcc -c *.c 
+
+g++ -c *.cpp 
+
+g++ *.o -lgdi32 -lole32 -loleaut32 -luuid -lwinmm -lmsimg32 -o sprite.exe
+
+
+
+
+
+gcc -c *.c && g++ -c *.cpp && g++ *.o -lgdi32 -lole32 -loleaut32 -luuid -lwinmm -lmsimg32 -o sprite.exe
+
